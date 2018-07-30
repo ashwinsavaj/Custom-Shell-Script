ef15325b2e2550c1c1805e4bc030461e4331b199
Ashwin Savaj <41853884+ashwinsavaj@users.noreply.github.com>
2018-07-30 10:56:04 +0530

    Update Custom_shell_script.sh

--- Custom_shell_script.sh
+++ Custom_shell_script.sh
@@ -139,7 +139,14 @@ sudo dpkg -i teamviewer_12.0.xxxxx_i386.deb
 
 #Code Blocks
 
+  sudo apt-get update
   sudo apt-get install codeblocks
+ 
+#chrome installation
+	
+	sudo apt-get install libxss1 libappindicator1 libindicator7
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        sudo dpkg -i google-chrome*.deb
 
 
 
